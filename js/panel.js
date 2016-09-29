a87647f3e62b63ae54aa95f8994011765dbae242
unknown <mbasha@IN-L0815.corp.yodlee.com>
2016-09-29 22:17:31 +0530

    Added timeout to event driven testcases

--- js/panel.js
+++ js/panel.js
@@ -27,6 +27,17 @@
     ];
     domAgent.init("POLL_RES");
     myApp = angular.module('myApp',['ngRoute']);
+    myApp.filter('timerSum', function() {
+        return function(input) {
+            var len = input.length,
+                out = 0,
+                i;
+            for (i = 0; i < len; i++) {
+                out += input[i].timer * 1000;
+            }
+            return out + 1000;
+        }
+    });
     myApp.filter('getEventName', function() {
         return function(input) {
             var out = "click";
